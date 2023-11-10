459a1e469014c17edef46be4068566c2e52da7c3
Matejkob <Matejkob@users.noreply.github.com>
2023-11-10 19:28:45 +0000

    Run swift-format

--- Examples/Sources/ViewModel.swift
+++ Examples/Sources/ViewModel.swift
@@ -2,39 +2,39 @@ import Spyable
 
 @Spyable
 protocol ServiceProtocol {
-    var name: String { get }
-    var anyProtocol: any Codable { get set }
-    var secondName: String? { get }
-    var added: () -> Void { get set }
-    var removed: (() -> Void)? { get set }
+  var name: String { get }
+  var anyProtocol: any Codable { get set }
+  var secondName: String? { get }
+  var added: () -> Void { get set }
+  var removed: (() -> Void)? { get set }
 
-    func initialize(name: String, _ secondName: String?)
-    func fetchConfig(arg: UInt8) async throws -> [String: String]
-    func fetchData(_ name: (String, count: Int)) async -> (() -> Void)
+  func initialize(name: String, _ secondName: String?)
+  func fetchConfig(arg: UInt8) async throws -> [String: String]
+  func fetchData(_ name: (String, count: Int)) async -> (() -> Void)
 }
 
 final class ViewModel {
-    private let service: ServiceProtocol
+  private let service: ServiceProtocol
 
-    var config: [String: String] = [:]
+  var config: [String: String] = [:]
 
-    init(service: ServiceProtocol) {
-        self.service = service
+  init(service: ServiceProtocol) {
+    self.service = service
+  }
+
+  func initializeService(with name: String) {
+    service.initialize(name: name, nil)
+  }
+
+  func saveConfig() async throws {
+    if config.isEmpty {
+      let result = try await service.fetchConfig(arg: 1)
+      config = result
+
+      return
     }
 
-    func initializeService(with name: String) {
-        service.initialize(name: name, nil)
-    }
-
-    func saveConfig() async throws {
-        if config.isEmpty {
-            let result = try await service.fetchConfig(arg: 1)
-            config = result
-
-            return
-        }
-
-        _ = try await service.fetchConfig(arg: 2)
-        config.removeAll()
-    }
+    _ = try await service.fetchConfig(arg: 2)
+    config.removeAll()
+  }
 }
--- Examples/Tests/ViewModelTests.swift
+++ Examples/Tests/ViewModelTests.swift
@@ -1,63 +1,64 @@
-@testable import Examples
 import XCTest
 
+@testable import Examples
+
 final class ViewModelTests: XCTestCase {
-    var serviceSpy: ServiceProtocolSpy!
-    var sut: ViewModel!
+  var serviceSpy: ServiceProtocolSpy!
+  var sut: ViewModel!
 
-    override func setUp() {
-        super.setUp()
-        serviceSpy = ServiceProtocolSpy()
-        sut = ViewModel(service: serviceSpy)
+  override func setUp() {
+    super.setUp()
+    serviceSpy = ServiceProtocolSpy()
+    sut = ViewModel(service: serviceSpy)
+  }
+
+  func testInitializeService() {
+    let serviceName = "service_name"
+
+    sut.initializeService(with: serviceName)
+
+    XCTAssertTrue(serviceSpy.initializeNameCalled)
+    XCTAssertEqual(serviceSpy.initializeNameReceivedArguments?.name, serviceName)
+  }
+
+  func testSaveConfig() async throws {
+    let expectedConfig = ["key": "value"]
+
+    serviceSpy.fetchConfigArgReturnValue = expectedConfig
+
+    try await sut.saveConfig()
+
+    XCTAssertEqual(sut.config, expectedConfig)
+
+    XCTAssertEqual(serviceSpy.fetchConfigArgCallsCount, 1)
+    XCTAssertEqual(serviceSpy.fetchConfigArgReceivedInvocations, [1])
+
+    try await sut.saveConfig()
+
+    XCTAssertTrue(sut.config.isEmpty)
+
+    XCTAssertEqual(serviceSpy.fetchConfigArgCallsCount, 2)
+    XCTAssertEqual(serviceSpy.fetchConfigArgReceivedInvocations, [1, 2])
+  }
+
+  func testThrowableError() async throws {
+    serviceSpy.fetchConfigArgThrowableError = CustomError.expected
+
+    do {
+      try await sut.saveConfig()
+      XCTFail("An error should have been thrown by the sut")
+    } catch CustomError.expected {
+      XCTAssertEqual(serviceSpy.fetchConfigArgCallsCount, 1)
+      XCTAssertEqual(serviceSpy.fetchConfigArgReceivedInvocations, [1])
+      XCTAssertTrue(sut.config.isEmpty)
+    } catch {
+      XCTFail("Unexpected error catched")
     }
-
-    func testInitializeService() {
-        let serviceName = "service_name"
-
-        sut.initializeService(with: serviceName)
-
-        XCTAssertTrue(serviceSpy.initializeNameCalled)
-        XCTAssertEqual(serviceSpy.initializeNameReceivedArguments?.name, serviceName)
-    }
-
-    func testSaveConfig() async throws {
-        let expectedConfig = ["key": "value"]
-
-        serviceSpy.fetchConfigArgReturnValue = expectedConfig
-
-        try await sut.saveConfig()
-
-        XCTAssertEqual(sut.config, expectedConfig)
-
-        XCTAssertEqual(serviceSpy.fetchConfigArgCallsCount, 1)
-        XCTAssertEqual(serviceSpy.fetchConfigArgReceivedInvocations, [1])
-
-        try await sut.saveConfig()
-
-        XCTAssertTrue(sut.config.isEmpty)
-
-        XCTAssertEqual(serviceSpy.fetchConfigArgCallsCount, 2)
-        XCTAssertEqual(serviceSpy.fetchConfigArgReceivedInvocations, [1, 2])
-    }
-
-    func testThrowableError() async throws {
-        serviceSpy.fetchConfigArgThrowableError = CustomError.expected
-
-        do {
-            try await sut.saveConfig()
-            XCTFail("An error should have been thrown by the sut")
-        } catch CustomError.expected {
-            XCTAssertEqual(serviceSpy.fetchConfigArgCallsCount, 1)
-            XCTAssertEqual(serviceSpy.fetchConfigArgReceivedInvocations, [1])
-            XCTAssertTrue(sut.config.isEmpty)
-        } catch {
-            XCTFail("Unexpected error catched")
-        }
-    }
+  }
 }
 
 extension ViewModelTests {
-    enum CustomError: Error {
-        case expected
-    }
+  enum CustomError: Error {
+    case expected
+  }
 }
